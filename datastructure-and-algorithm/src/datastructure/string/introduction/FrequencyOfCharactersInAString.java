5d01093f22a485f7df62cf94e00ee1676007a048
rkumari110 <kumari.rinki1110@gmail.com>
2022-11-11 12:12:56 +0530

    Palindrome check

--- datastructure-and-algorithm/src/datastructure/string/introduction/FrequencyOfCharactersInAString.java
+++ datastructure-and-algorithm/src/datastructure/string/introduction/FrequencyOfCharactersInAString.java
@@ -1,32 +1,28 @@
 package datastructure.string.introduction;
 
-
 public class FrequencyOfCharactersInAString {
-	
+
 	public static void main(String args[]) {
 		printFrequencies("geeksforgeeks");
 	}
-	
-	private static void printFrequencies (String s) {
-		
-		final int MAX_CHAR = 26; 
-		int count [] =  new int [MAX_CHAR];
-		
+
+	private static void printFrequencies(String s) {
+
+		final int MAX_CHAR = 26;
+		int count[] = new int[MAX_CHAR];
+
 		for (int i = 0; i < s.length(); i++) {
-			
+
 			count[s.charAt(i) - 'a']++;
-			
+
 		}
-		
+
 		for (int i = 0; i < MAX_CHAR; i++) {
-			if (count[i]>0) {
+			if (count[i] > 0) {
 				System.out.println((char) (i + 'a') + " " + count[i]);
 
-				
 			}
 		}
-		
-		
 	}
 
 }
---
+++ datastructure-and-algorithm/src/datastructure/string/palindromecheck/PalindromeCheckForAString.java
@@ -0,0 +1,9 @@
+package datastructure.string.palindromecheck;
+
+public class PalindromeCheckForAString {
+	
+	public static void main(String[] args) {
+
+	}
+
+}
